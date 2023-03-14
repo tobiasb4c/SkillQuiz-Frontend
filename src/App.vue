ae393b7b659d5e8e7380ced7f15ccf5a0b288340
NikolaPetkovski <9046@htl.rennweg.at>
2023-03-14 13:26:39 +0100

    Mockup erstellt + function angelegt

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script>
+
+import Timer from './components/Timer.vue'
+
+export default{
+    components:{
+        Timer
+    }
+}
 </script>
 
 <template>
+
+    <Timer minute="11" second="23"/>
 
 </template>
 
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,13 +1,35 @@
 <script>
+import { onMounted } from 'vue';
 
-export default {    
-    props:{
+
+export default {
+    data() {
+        return {
+            minute: this.minute,
+            second: this.second,
+        }
+    },
+    props: {
         minute: Number,
         second: Number
     },
     methods: {
-        restTime(){
-            return this.minute + " Minutes " + this.second + " Seconds"
+        startTimer() {
+            console.log("w")
+            this.second = 20
+            /*setInterval(function(){
+                
+                this.second--
+
+                if(this.second == 0){
+                    this.minute--
+                    this.second = 60
+                }
+                if(this.minute == 0 && this.second == 0){
+                    console.log("Beendet")
+                }
+            }, 1000)
+            */
         }
     }
 }
@@ -17,24 +39,27 @@ export default {
 </script>
 
 <template>
-    <div class="">
-        <p>Time until exam closes</p>
-        <p> {{ restTime() }}</p>
+    <div class="flex flex-col items-center m-10">
+        <p class="mt-2">Time until exam closes</p>
+        <p class="mb-2"> {{ this.minute }} Minutes {{ this.second }} Seconds</p>
+        <button @click="startTimer">Start</button>
     </div>
 </template>
 
 <style scoped>
-
 @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 
 
 div {
     background-color: white;
-    position: absolute;
     width: 573px;
     height: 102px;
     box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.1);
     border-radius: 5px;
+}
+
+p:last-child {
+    color: #828282;
 }
 
 p {
@@ -46,8 +71,7 @@ p {
     font-weight: 400;
     font-size: 24px;
     line-height: 29px;
-    display: flex;
-    align-items: center;
     text-align: center;
+
 }
 </style>
