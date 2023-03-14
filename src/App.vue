63ef7792d1e914a44e3f22755eb1702f0b1eb51a
tobiasb4c <tobias.nahler@b4c-consulting.com>
2023-03-14 12:00:24 +0100

    dev finished
    waiting for tests

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script>
+import Titel from './components/Titel.vue'
+
+export default{
+    components: {
+        Titel
+    }
+}
 </script>
 
 <template>
-
+    <Titel titel="Hallo Welt" />
 </template>
 
 <style scoped>
