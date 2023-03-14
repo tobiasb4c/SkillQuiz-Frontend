f83c26284b25e91c96ec1d3856853308cbc9cb16
Fabian Gesswagner <Fabian.gesswagner@htl.rennweg.at>
2023-03-14 11:54:35 +0100

    finished component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script>
+import Frage from './components/Frage.vue';
+
+export default  {
+  components: {
+    Frage
+  },
+  data() {
+    return {
+    }
+  },
+}
 </script>
 
 <template>
-
+<Frage text="Hallo Test Frage" class="w-full"/>
 </template>
 
 <style scoped>
---
+++ src/components/Frage.vue
@@ -0,0 +1,27 @@
+<script>
+export default  {
+    props: {
+        text: String,
+    }
+}
+</script>
+
+<template>
+    <div class="flex justify-center">
+        <h2>{{ text }}</h2>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+
+h2  {
+    font-family: 'Lato', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 29px;
+    text-align: center;
+    color: #232323;
+}
+</style>
