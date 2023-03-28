76d0e6fe9d653d612140593bb30aedae892dfc0b
Nikola Petkovski <105193900+NikolaPetkovski@users.noreply.github.com>
2023-03-28 11:29:28 +0200

    Nikola zaubert

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script>
+
+import Skip from './components/Skip.vue';
+
+export default {
+    components: {
+        Skip
+    }
+
+
+}
 </script>
 
 <template>
-
+    <Skip />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/Skip.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default{
+
+}
+
+</script>
+
+<template>
+
+<button name="previous" class="">Previous</button>
+<button name="next">Next</button>
+
+</template>
+
+<style scoped>
+
+</style>
