9ba28894930f1545ffc2ce53f1cef159da0de0ed
NikolaPetkovski <9046@htl.rennweg.at>
2023-03-28 12:14:24 +0200

    Buttons gestyled

--- src/components/Skip.vue
+++ src/components/Skip.vue
@@ -1,18 +1,29 @@
 <script>
 
-export default{
+export default {
 
 }
 
 </script>
 
 <template>
-
-<button name="previous" class="">Previous</button>
-<button name="next">Next</button>
-
+    <div class="grid grid-cols-2 grid-rows-1 gap-1">
+        <button class="my-1 py-1 px-10 text-white">Previous</button>
+        <button class="my-1 py-1 px-10 text-white">Next</button>
+    </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 
+button {
+    background-color: #2FB4BC;
+    border-radius: 3px;
+
+    font-family: 'Lato', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 29px;
+}
 </style>
